133d133f9f68822c648ce15e18beedc2e0b1cc7a
Andrii Melnichuk <andrewlutsk88@gmail.com>
2016-10-14 11:28:10 +0300

    make 3dtour controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,6 +44,7 @@ Route::group(['prefix'=>'admin30x5', 'middleware' => 'auth'], function(){
 
 Route::group(['middleware' => 'frontend.init'], function(){
 	Route::get('/{lang}/booking', 'Frontend\BookingController@index');
+	Route::get('/{lang}/3dtour', 'Frontend\TourController@index');
 	Route::get('/{lang}/{type?}', 'Frontend\ArticleController@index')->where('type', 'hotel|rooms|services|events|gallery|contact|3dtour');;
 });
 
