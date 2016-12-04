d53512114f2d99fbca6c06c914535dc4f32234ed
Andrii Melnichuk <andrewlutsk88@gmail.com>
2016-12-04 14:15:28 +0200

    make link on services img on main

--- resources/views/FrontEnd/hotel.blade.php
+++ resources/views/FrontEnd/hotel.blade.php
@@ -2,14 +2,10 @@
 
 @section('content')
 
-
-
-
-
     <div class="slider">
 
-
         <div id="webstudio-slider" style="display:none;">
+
             @foreach($slides as $slide)
 
                 @if(count($slide->getImages()) > 0)
@@ -17,25 +13,15 @@
                         <img alt="{{$slide->getTranslate('title')}}" src="/{{$slide->getImages()[0]['full']}}"
                         data-image="/{{$slide->getImages()[0]['full']}}"
                         id="img-{{$slide->id}}"
-                        data-description=" {{$slide->getTranslate('description')}}"
-                        data-link="jhjhjhj111">
+                        data-description=" {{$slide->getTranslate('description')}}">
 
                 @endif
             
             @endforeach
+
         </div>
 
     </div>
-
-    {{--<div id="3d-tour">
-    </div>
-    <script>
-        document.addEventListener("DOMContentLoaded", function(){
-            $(function(){
-                $('#3d-tour').html('<iframe src="http://www.visitor.ee/360/pc/html5/main.html" style="width: 100%; height: 470px; border: none; margin-bottom: -5px" frameborder="none"></iframe>');
-            });
-        });
-    </script>--}}
 
     <div class="booking clearfix">
 
@@ -62,22 +48,22 @@
 
     </div>
 
-
     <div class="content-main">
 
+        <span>{{ trans('base.hotelcontenttitle') }}</span>
 
-    <span>{{ trans('base.hotelcontenttitle') }}</span>
-
-    <ul class="clearfix">
+        <ul class="clearfix">
 
             @foreach($services as $service)
 
                 <li>
+
                     @if(count($service->getImages()) > 0)
-                    <div class="img-block" style="background: url('/{{ $service->getImages()[0]['min'] }}') no-repeat center; background-size: cover"></div>
+                        <a href="/{{ App::getLocale() }}/services/#service-{{ $service -> id }}"><div class="img-block" style="background: url('/{{ $service->getImages()[0]['min'] }}') no-repeat center; background-size: cover"></div></a>
                     @else
-                    <div class="img-block"></div>
+                        <div class="img-block"></div>
                     @endif
+
                     <div class="describe-block">
 
                         <h1>{{ str_limit($service -> getTranslate('title'),20,'...') }}</h1>
@@ -92,14 +78,14 @@
 
             @endforeach
 
-    </ul>
+        </ul>
 
-    <div class="hotel-describe">
+        <div class="hotel-describe">
 
-    <h1>{{ $hotel -> getTranslate('title') }}</h1>
-    <section>{!!  $hotel -> getTranslate('description') !!}</section>
+            <h1>{{ $hotel -> getTranslate('title') }}</h1>
+            <section>{!!  $hotel -> getTranslate('description') !!}</section>
 
-    </div>
+        </div>
 
     </div>
 
